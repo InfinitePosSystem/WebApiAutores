33a73d0522b49508e586901c98a1d86923816d19
Daniel Rincon <wordmontain123@gmail.com>
2023-12-29 23:28:33 -0500

    se añaden reglas de validacion a Autores

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,9 +1,25 @@
-﻿namespace WebApiAutores.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApiAutores.Validaciones;
+
+namespace WebApiAutores.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "el campo {0} es requerido")]
+        [StringLength(maximumLength:10, ErrorMessage ="El campo {0} no debe tener más de {1} carácteres")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
+        [Range(18,120)]
+        [NotMapped]
+        public int Edad { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string TarjetaDeCredito { get; set; }
+        [Url]
+        [NotMapped]
+        public string Url { get; set; }
         public List<Libro> Libros { get; set; } 
 
     }
