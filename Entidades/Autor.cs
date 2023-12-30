2681b04a5210688b5b25335d8bcc7f49882a5ee7
Daniel Rincon <wordmontain123@gmail.com>
2023-12-29 23:42:45 -0500

    se añaden reglas de validacion por modelo a Autores

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -4,23 +4,46 @@ using WebApiAutores.Validaciones;
 
 namespace WebApiAutores.Entidades
 {
-    public class Autor
+    public class Autor : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "el campo {0} es requerido")]
         [StringLength(maximumLength:10, ErrorMessage ="El campo {0} no debe tener más de {1} carácteres")]
-        [PrimeraLetraMayuscula]
-        public string Nombre { get; set; }
-        [Range(18,120)]
-        [NotMapped]
-        public int Edad { get; set; }
-        [CreditCard]
-        [NotMapped]
-        public string TarjetaDeCredito { get; set; }
-        [Url]
-        [NotMapped]
-        public string Url { get; set; }
-        public List<Libro> Libros { get; set; } 
+        //[PrimeraLetraMayuscula]
 
+        public string Nombre { get; set; }
+        //[Range(18,120)]
+        //[NotMapped]
+        //public int Edad { get; set; }
+        //[CreditCard]
+        //[NotMapped]
+        //public string TarjetaDeCredito { get; set; }
+        //[Url]
+        //[NotMapped]
+        //public string Url { get; set; }
+        public List<Libro> Libros { get; set; }
+        [NotMapped]
+        public int Mayor { get; set; }
+        [NotMapped]
+        public int Menor { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe se mayuscula",
+                        new string[] { nameof(Nombre) });
+                }
+            }
+            if (Mayor <= Menor)
+            {
+                yield return new ValidationResult("el valor Mayor debe ser mayor a Menor",
+                    new string[] { nameof(Mayor) });
+            }
+            
+        }
     }
 }
